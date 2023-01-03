ed4b265721f4819d972b5b5ffc07be2136471765
prashantmali <maliprashant1911@gmail.com>
2023-01-03 16:41:52 +0530

    docker file added

--- src/main/java/com/prashant/github/githubaction/GithubActionApplication.java
+++ src/main/java/com/prashant/github/githubaction/GithubActionApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GithubActionApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GithubActionApplication.class, args);
 	}
 
